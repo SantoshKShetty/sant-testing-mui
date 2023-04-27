206c24da04c71f17fbceeccaa161429abcfc173c
Santosh Shetty <santosh.shetty@PNQM1004-SASHET.local>
2023-04-27 19:10:37 +0530

    inint

---
+++ src/components/textfield/textfield.js
@@ -0,0 +1,5 @@
+import TextField from "@mui/material/TextField";
+
+export default function Textfield(props) {
+    return <TextField {...props} />
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as SolidButton } from "./components/button/solid";
+export { default as OutlinedButton } from "./components/button/outlined";
+
+export { default as TextField } from "./components/textfield/textfield";
